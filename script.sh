0f8c662a38a15ec34eef924295ecb8c03a8d6ae6
mnaghd01 <mnaghd01@students.poly.edu>
2018-01-31 13:50:46 -0500

    scrip updated

--- script.sh
+++ script.sh
@@ -25,40 +25,46 @@ echo "Baseboard Information:"
 dmidecode -t baseboard | grep -iE 'Serial Number:| Part\ Number'
 
 printf '%*s\n'  "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo "The CPU info"
+echo "The Cpu "
 lscpu | grep 'socket'
 
 printf '%*s\n'  "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo "Number of CPUs"
+echo "Number of Cores"
 lscpu | grep '^CPU(s)'
 
 printf '%*s\n'  "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo "Number of CPU sockets"
-lscpu |grep '^Socket(s)'
+echo "Number of Threads per core"
+lscpu | grep 'Thread(s)'
+
+
+
+#printf '%*s\n'  "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
+#echo "Number of memory devices"
+#dmidecode -t memory | grep -c clock
+##lshw -class memory | grep -c '^Serial'
 
 printf '%*s\n'  "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo "Number of threads per core"
-lscpu | grep -i thread 
+echo "Numer of memory banks"
+#dmidecode -t memory | grep -c -o '^\tPart Number: (?!\[Empty\])'
+lshw -class memory |grep -c bank
 
 printf '%*s\n'  "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo "Number of memory devices"
-dmidecode -t memory | grep -c '^Memory Device$'
-
-printf '%*s\n'  "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo "Numer of empty slots"
-dmidecode -t memory | grep -c -o '^\tPart Number: (?!\[Empty\])'
+echo "Numer of Empty memory banks"
+#dmidecode -t memory | grep -c -o '^\tPart Number: (?!\[Empty\])'
+lshw -class memory |grep -c empty
 
 printf '%*s\n'  "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 echo "Memory Size, Type, Speed And Serial"
-dmidecode -t memory | grep -iE "Size\n|Type:|Speed|Serial|Part\ Number"
+#dmidecode -t memory | grep -iE "Size\n|Type:|Speed|Serial|Part\ Number"
+lshw -class memory | grep -iE 'serial|clock|size|vendor'
 
 printf '%*s\n'  "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
-echo "Number of Hard Drives"
+echo "Number of Hard Drive Bays"
 inxi -Dxx |grep "ID" |wc -l
 
 printf '%*s\n'  "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 echo "Hard Drives Capacity and Serial Number"
-inxi -Dxx
+lshw -class storage -short
 
 printf '%*s\n'  "${COLUMNS:-$(tput cols)}" '' | tr ' ' -
 echo "HBA Cards"
